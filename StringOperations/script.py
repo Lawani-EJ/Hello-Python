04fac9d84b90ac47ce39b3c18e183ca71009a45e
Lawani-EJ <elyonjohn60@gmail.com>
2024-05-14 09:43:24 +0100

    Retriveing even elements from a string For this commit i utilized string slicing to extract every second element from the given string, This operation selects the even elements found in the index, -------------------------------------------------------------------------------------------------- Using the find method in a string For this second commit i implemented the find method to locate the index of the first occurance , Of the specific character within the given string.

---
+++ StringOperations/script.py
@@ -0,0 +1,16 @@
+#Considering the following string
+#"01234567"
+
+Numbers = "0123456"
+
+#When obtaining the even elements
+#We can perform the string operations
+
+Numbers[::2]
+print(Numbers[::2])
+
+#Another sring manipulation can be performed 
+#In getting the result of the following line of code
+
+"0123456".find('1') 
+print("0123456".find('1'))
