e4d95df527e3f63736f57f3d4ede2b1eb8890073
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-15 19:14:27 +0100

    Refactoring and extending list operations In this commit i inroduced a sample list containing various data types, including string, float, integer, list, and tuple to demonstrate python's versatality in handling hetrogenous data types, Implemented slicing on the slicing list to extract specific elements from the list Utilzed the extend method to append multiple elements to the extendList These updates enhances the script by illustrating the fundamental list operations in python This commit provides a comprehnesive example of handling and manipulating lists.

--- List-and-Tuples/Lists/script.py
+++ List-and-Tuples/Lists/script.py
@@ -1,5 +1,16 @@
 List = ["Micheal Jackson" ,10.1,1982]
 print(List)
+print(type(List))
 print("Printing the same elements using negative and positive indexing: \n Positive index:",List[0], "\n Negative Index:",List[-3])
 print("Printing the same elements using negative and positive indexing: \n Positive index:",List[1], "\n Negative Index:",List[-2])
 print("Printing the same elements using negative and positive indexing: \n Positive index:",List[2], "\n Negative Index:",List[-1])
+
+sampleList = ["Micheal Jackson", 10.1, 1982, [1,2], ("A",1)]
+print(sampleList)
+slicingList = ["Micheal Jackson", 10.1, 1982, "MJ", 1]
+print(slicingList[3:5])
+
+extendList = ["Michaeal Jackson", 10.2]
+extendList.extend(['pop', 10])
+print(extendList)
+
