8543afec40f7956d3d35ed3eda0906544351d2e5
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-16 17:30:36 +0100

    Enhanced the python list operations and string handling examples Appended the sublist  to Modified the first element of  from  to Demonstrated the deletion of the first element in Split  into a list using Created a shallow copy of  and demonstrated the impact of changes in the .

--- List-and-Tuples/Lists/script.py
+++ List-and-Tuples/Lists/script.py
@@ -14,3 +14,30 @@ extendList = ["Michaeal Jackson", 10.2]
 extendList.extend(['pop', 10])
 print(extendList)
 
+appendList = ["Michaeal Jackson", 10.2]
+appendList.append(["a","b","c"])
+print(appendList)
+
+changeList = ["disco", 10, 1.2]
+print("Before change:", (changeList))
+changeList[0] = "funk"
+print("After Change:",(changeList))
+print("Before delete:", (changeList))
+del(changeList[0])
+print("After Delete:",(changeList))
+
+splitString = "Hello,World,Python"
+print(splitString.split(','))
+print(type(splitString.split(',')))
+
+copyList = ["hard Rock", 10, 1.3]
+copiedList = copyList
+print("Copy the list",copyList)
+print("copied List",copiedList)
+print ("copiedList[0]",copiedList[0])
+copyList[0] = "Bannana"
+print ("copiedList[0]",copiedList[0])
+
+print ("copiedList[0]",copiedList[0])
+copyList[0] = "hard rock"
+print ("copiedList[0]",copiedList[0])
