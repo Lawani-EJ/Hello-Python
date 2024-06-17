9a038b9f63cdf98a7c8ddd06bb5ff165ece545a3
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-17 23:11:38 +0100

    Added and demonstrated set Operations for music genres and album details This commit introduces the use of sets to manage and manipulate collection of music genres and album details Sets are employed to eleiminate duplicates and demonstrate the unique elements within each collection For the music genres set iniitilazation A set1 is created to store various music genres including pop , rock, soul,hard rock r&b and disco. this set automatically removes duplicate occurances of rock Album details conversion to set An album details initially stored in a list format are converted into a set named album_set. This conversion highlights the unique elements of the album's metadata, filtering out any repititive data This commit effectively demonstrates the use of sets to streamline and manage unique data elements, facilitating data handling and processing.

---
+++ Sets/script.py
@@ -0,0 +1,7 @@
+set1 = {"pop","rock","soul","hard rock","rock","R&B","rock","disco"}
+print(set1)
+album_list = ["Micheal Jackson", "Thriller",1982,"00:42:19","pop, Rock, R&B",46.0,65,"30-Nov-82", None,10.0]
+album_set = set(album_list)
+print(album_set)
+Music_genres = set(["pop","pop","rock","folk rock","hard rock","soul","progressive rock","soft rock","R&B","disco"])
+print(Music_genres)
