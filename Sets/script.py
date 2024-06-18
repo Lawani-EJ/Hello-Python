7a040d09a83e5dbed0e0e47aca8c1f2f00371bea
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-18 18:02:54 +0100

    Sets Operations in python (Practice) This practice provided a comprehensive overview of the basic set operaitons Reinforcing the concepts of sets as unordered collections of unique elements and their utility in performing mathematical set operations Such as Union, Intersection, and Difference. Thank you git add script.pygit add script.pygit add script.py!

--- Sets/script.py
+++ Sets/script.py
@@ -5,3 +5,26 @@ album_set = set(album_list)
 print(album_set)
 Music_genres = set(["pop","pop","rock","folk rock","hard rock","soul","progressive rock","soft rock","R&B","disco"])
 print(Music_genres)
+
+SampleSet = set(["Thriller", "Back in Black", "AC/DC"])
+print(SampleSet)
+SampleSet.add("NYC")
+SampleSet.add("NYC")
+SampleSet.add("NYC")
+print(SampleSet)
+SampleSet.remove("NYC")
+print(SampleSet)
+"Thriller" in SampleSet
+print("Thriller" in SampleSet)
+
+album_set1 = set(["Thriller","AC/DC","Back In Black"])
+album_set2 = set(["AC/DC","Back In Black", "The Dark Side of The Moon"])
+print(album_set1,album_set2)
+
+FindIntersection = album_set1 & album_set2
+print(FindIntersection)
+
+print(album_set1.difference(album_set2))
+print(album_set2.difference(album_set1))
+
+print (album_set1.intersection(album_set2))
