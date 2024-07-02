749aa8d211ba582bda0102c215c0c0113ccc65a6
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-02 14:39:43 +0100

    Demonstrating comparisons examples This commit demonstrates the various comparisons operations in python (==)Compares integers using equality and (!=) for inequality I've evaluated boolean expressions with the relational operators (>) I've shown string and character comparisons based on ASCII values Understanding comparisons operaitons is important for programming Because they allow the control of flow based on conditions, validation of inputs to ensure proper data handling in applications.

---
+++ Conditions-Branching/Condition/script.py
@@ -0,0 +1,21 @@
+a = 5
+print(a == 6)
+# print(a)
+
+i = 6
+print(i>5)
+j = 2
+print(j>5)
+
+k = 2
+print(k!=6)
+
+l = 6
+print(l!=6)
+
+print("AC/DC" == "Micheal Jackson")
+print("AC/DC" != "Micheal Jackson")
+
+print('A'>'B')
+print('B'>'A')
+print('BA'>'AB')
