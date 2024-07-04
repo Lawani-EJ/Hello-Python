91c0b7c441a454f8e05819559e488d0ec096dd91
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-04 14:52:03 +0100

    Conditional refactoring for user eligibity and album year checks In this commit i began by updating the age validation logic to help prevent users from under 18 to not enter the concert I've enhanced user eligibilty cheking for website access Refining the album year comparisons to help determine if an album's release year is greater or lesser.

---
+++ Conditions-Branching/Branching/script.py
@@ -0,0 +1,36 @@
+# age = 19
+age = 17
+if age < 18:
+    print("You cannot Enter the Concert")
+    # print("You can Enter")
+
+user_age = 18
+if user_age > 18:
+    print("You are eligible to visit this website")
+else:
+    print("You are not eligible to vist the site")
+
+    user2_age = 15 
+    if user2_age > 18:
+        print("You are eligible to visit this website")
+    elif user2_age == 18:
+        print("You are eligible to visit this website proceed to login!!!")
+    else:
+        print("You are not eligible to visit this website")
+        print("Exit page!!")
+
+
+album_year = 1983
+album_year = 1970
+
+if album_year > 1980:
+    print("Album year is greater than 1980")
+    print ("Do something")
+
+
+album2_year = 1985
+if album2_year > 1980:
+        print("Album year is greater than 1980")
+else :
+     print("This album is less than 1980")
+     print("Do something")
