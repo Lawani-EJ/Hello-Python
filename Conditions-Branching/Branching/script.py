efa91424ea4c47bf5c8da980568e5b14d0a7649a
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-10 13:00:40 +0100

    Checking the leap-year logic and refactoring the age-based meal application In this commit i have implemented the logic for determining the age of annie and jane to determine who was born in a leap year I've correctly identified the leap years using the modulo operation And added appropiate messages for each case Enhancements for the breakfast assignment logics for the school canteen In this commit i began by fixing the age comparisons conditions to ensure that the meals are all allocated properly Ensuring a clear and simple message for every meal being allocated.

--- Conditions-Branching/Branching/script.py
+++ Conditions-Branching/Branching/script.py
@@ -50,3 +50,42 @@ else :
 album5_year = 2024
 if not (album5_year == 2021):
      print("Album 5's year is not 2021 it's 2024")
+
+#given quiz
+#There are 2 sisters Annie and Jane born in 1996 amd 1999 respectively. They want to know who was born in a leap year Write an if else statement to determine  who was born in a leap year
+Annie = 1996
+Jane = 1999
+
+if Annie %4 == 0:
+     print("Annie was born in a leap year!")
+elif Jane %4 == 0:
+    print("Jane was born in a leap year!")
+else:
+     print("None of them were born in a leap year!") 
+
+#quiz2
+# In a school canteen children under the age of 9 are only given milk porridge  for breakfast children from 10-14 are given a sandwich and children from 15-17 are given a burger.
+# The canteen master asks the age of the student and gives them breakfast accordingly . Sam's age is 10 using an if-else statement dtermine what the canteen master will serve him 
+#Solution
+Milk_porridge = 9
+Sandwich = 14
+Burger = 17
+
+Sam = 10
+
+if Sam > Milk_porridge:
+     print("Sam takes milk porridge for breakfast!")
+elif Sam > Sandwich:
+     print("Sam takes Sanwich for brakfast!")
+elif Sam > Burger:
+     print("Sam Takes Burger for beakfast!")
+
+# Correction
+age = 10
+
+if age <= 9:
+    print("You will get a bowl of porridge!")
+elif age >=10 and age <= 14:
+     print("You will get a sandwich!")
+elif age >= 15 and age <= 17:
+    print("You will get a burger!")
