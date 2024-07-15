0a913a278698a3032e8c192ab01d26e38766a69f
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-15 14:40:03 +0100

    Practicing iterating on loops Inthis commit i have added sevral examples adding robust iteration I began by iterating through the dates list and also printed a few numbers In the next example i practiced updating the square list And printed clear statements after each succesful iteration

--- Loops/script.py
+++ Loops/script.py
@@ -9,3 +9,23 @@ y = 1
 while(y!=x):
     print(y)
     y = y+1
+
+dates = [1980,1982,1973]
+N = len (dates)
+for i in range (N):
+    print(dates[i])
+
+    for x in range(0,8):
+        print(x)
+
+for year in dates:
+    print(year)
+
+squares = ["red","Yellow","Green","Blue"]
+for i in range(0,5):
+    print("Before number square: ", i , "was" , squares[i])
+    squares[i] = "white"
+    print("Now after number square: ", i, "is", squares[i])
+
+for i , square in enumerate(squares):
+    print(i,square)
