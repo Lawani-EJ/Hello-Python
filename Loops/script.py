291961bf3b5d2c0d518caebd066f1a1174c0bad4
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-20 14:10:03 +0100

    Class Quiz pratical on loops In this commit i am implementing basic loops that perform several iterations The development of a for loop that iterates that performs the iteration from a specific range (-4 and 5) printing every value within this range In the next function the creation of the list of a music genre list has been implemented and then the performance of a loop has been inserted to make it loop and print out each value in that list In the next function the creation of a playlist rating list has been created, after the creation of the list a condition has been attached to this list It continously prints the ratings in the list until a rating that is less than 6 is encountered and then the iteration stops. In the next function the creation of the square list has been implemented with an empty list called new_squares this will copy the orange strings that are in the square list into the empty list new_squares Then a condition is performed it will ensure that the loop exits when it encounters a string that isn't orange.

--- Loops/script.py
+++ Loops/script.py
@@ -29,3 +29,37 @@ for i in range(0,5):
 
 for i , square in enumerate(squares):
     print(i,square)
+
+# class quiz
+# Write a loop that iterates through the elements of 5 and -5
+
+for i in range(-4,5):
+    print(i)
+
+#create a list and print out the elements in that list using a for loop
+Genres = ['rock', 'R&B', 'Soundtrack', 'R&B', 'soul', 'pop']
+print(type(Genres))
+
+for genre in Genres:
+    print(genre)
+
+#Create a while loop that displays all the values for the rating of an album playlist's rating list
+PlaylistRatings = [10,9.5,10,8,7.5,5,10,10]
+#if the score is less than 6 exit the loop 
+i = 0
+rating =  PlaylistRatings[0]
+while(i < len(PlaylistRatings) and rating >=6):
+    print(rating)
+    i = i+1
+    rating = PlaylistRatings[i]
+    
+
+# Create a while loop that will copy the strings 'orange' of this given list squares to the provided list that is given below 
+squares = ['orange', 'orange', 'purple', 'blue', 'orange']
+new_squares = []
+# make sure it exits the loop if the value on the list isn't orange
+i = 0
+while(i < len(squares) and squares[i] == 'orange'):
+    new_squares.append(squares[i])
+    i = i + 1
+    print(new_squares) 
