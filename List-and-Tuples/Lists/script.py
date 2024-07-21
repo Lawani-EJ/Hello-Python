2b89760c1d341fef89598e4323cc3deccc0808ee
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-21 21:49:39 +0100

    My solutions to a number of python programming exercises,meant for learning basic data structure manipulations The list of the tuples sorted, the tuples were sorted according to their last elements using a lamba function as a key. Combining dictonaries, combined the two dictonaries and made sure the values for their shared keys are added up. Multiplying the list items, to determine the product of all the components in a list utilized the math.prod function Sorting tuplees by float element,sorted the list of tuples in descending order by setting the second element into a float Set Operations, Used fundemental set operations such as iteration the addition components and the removal of components.

--- List-and-Tuples/Lists/script.py
+++ List-and-Tuples/Lists/script.py
@@ -41,3 +41,44 @@ print ("copiedList[0]",copiedList[0])
 print ("copiedList[0]",copiedList[0])
 copyList[0] = "hard rock"
 print ("copiedList[0]",copiedList[0])
+
+# Class questions
+# Question 2
+# Write a Python program to get a list, sorted in increasing order by the last element in each tuple, from a given list of non-empty tuples.
+Sample_List = ([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]) 
+Sample_List.sort(key = lambda x:x[-1])
+print(Sample_List)
+
+# Question 3 
+# Write a Python program that combines two dictionaries by adding values for common keys.
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+
+CombinedDictionary = (d1 | d2)
+print(CombinedDictionary)
+
+# Question 1 
+# Write a Python program that multiplies all the items in a list.
+import math
+Sample_list= [2, 3, 6]
+print(math.prod(Sample_list))
+
+# Question 5
+# Write a program to sort a tuple by its float element.
+# For example: 
+list= [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]
+list.sort(reverse=True)
+print(list)
+
+# Question 6
+# Write a Python program to create a set.
+SampleSets = {0,1,2,3,4,5}
+# Write a Python program to iteration over sets.
+for SampleSet in SampleSets:
+    print(SampleSet)
+# Write a Python program to add members in a set
+SampleSets.add("Micheal")
+SampleSets.add("Thriller")
+# and to remove items from a given set.
+SampleSets.remove("Thriller")
+print(SampleSets)
