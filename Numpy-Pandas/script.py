01b59306015cab0a3ca3a67ba7b1269fd552b241
Lawani-EJ <elyonjohn60@gmail.com>
2024-08-16 19:24:05 +0100

    Numpy introductory class In this commit i have created a python program for the creation of arrays I began by importing the numpy library I created a rank1,2 and 3 array with the use of numpy In the rank 3 array i implemented the creation of array using a tuple this shows the flexibility in input types.

---
+++ Numpy-Pandas/script.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+new_array=np.array([1,2,3])
+print("Array with rank 1: \n",new_array)
+new_array=np.array([[1,2,3],[4,5,6]])
+print("Array with rank 2: \n",new_array)
+new_array=np.array((1,2,3))
+print("Array with rank 3 that is created using a passed tuple: \n",new_array)
