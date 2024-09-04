21667ac734df35ecd336dd60dcd6eac1f72cec96
Lawani-EJ <elyonjohn60@gmail.com>
2024-09-04 16:11:37 +0100

    Writing multiple list items into the txt file In this commit i have created a list and stored it in a variable I proceeded to open the txt file and write a for loop that write the items of the list into that txt file, I then wrote a program that verifies if the items in that list have been written It opens the txt file and reads it for validations.

--- working_with_files/writing/script.py
+++ working_with_files/writing/script.py
@@ -1,8 +1,25 @@
 # writing a line to a file 
 exmp2 = 'Example2.txt'
 with open(exmp2, 'w') as writefile:
-    writefile.write("This is line A")
+    writefile.write("This is line A \n")
+
+# Writing multiple lines into that file 
+with open(exmp2, 'w') as writefile:
+    writefile.write("This is line B \n")
+    writefile.write("This is line C \n")
+    writefile.write("This is line D \n")
 
 # Reading the file checking it works
 with open(exmp2, 'r') as testWrite:
     print(testWrite.read())
+
+# Writing a list into that text file 
+Lines = ["This is list A \n", "This is list B \n", "This is list C \n",]
+with open('Example2.txt', 'w') as writefile:
+    for line in Lines:
+        # print(line)
+        writefile.write(line)
+
+# Verifying and reading if writing has been implemented successfully
+with open('Example2.txt', 'r') as testList:
+    print(testList.read())
