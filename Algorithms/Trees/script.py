54b427f7f68fef4e1faf4571c86780bb5a13bdf0
Lawani-EJ <elyonjohn60@gmail.com>
2024-09-21 13:07:57 +0100

    Provided the PSEUDOCODE for Tress Data Structures.

---
+++ Algorithms/Trees/script.py
@@ -0,0 +1,25 @@
+# PSEUDOCODE
+# search (value, root)
+# {
+#     if(root == null ){
+#         # Empty
+#         # Not found return null 
+#     }
+#     if(root.data == value){
+#         # Found 
+#         return root
+#     }
+#     if(root.data > value){
+#         return search(value,root.left)
+#     }
+#     if(root.data  < value){
+#         return search(value,root.right)
+#     }
+# }
+
+# class Node:
+#     def__init__(self,data):
+#          self.data = data
+#         self.children = []
+
+
