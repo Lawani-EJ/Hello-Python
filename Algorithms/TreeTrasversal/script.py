4d7bf8b05fbe3eb5d033ecb2622e773c887ee1b7
Lawani-EJ <elyonjohn60@gmail.com>
2024-09-23 14:24:49 +0100

    Implementation of pre-order tree trasversal.

--- Algorithms/TreeTrasversal/script.py
+++ Algorithms/TreeTrasversal/script.py
@@ -54,3 +54,33 @@
 #     PostOrder(node.right)           Trasverse right subtree
 #     visit(node)                     Process the current node
 
+# IMPLEMENTATION 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def pre_order(node):
+    if node is not None:
+        print(node.data, end=" ")
+        pre_order(node.left)
+        pre_order(node.right)
+
+    #     1
+    #    / \
+    #   2   3
+    #  / \ / \
+    # 4  5 6  7
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+print("Pre-Order Trasversal: ")
+pre_order(root)
