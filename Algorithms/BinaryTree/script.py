29d0582e4ea949bc53fac4a884b2be7127ed0e33
Lawani-EJ <elyonjohn60@gmail.com>
2024-09-25 13:06:19 +0100

    Performed a basic operation on binary trees with python.

--- Algorithms/BinaryTree/script.py
+++ Algorithms/BinaryTree/script.py
@@ -28,3 +28,60 @@
 #                 return
 #             else:
 #                 enqueue(node.right)
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+
+    def insert(self, data):
+        new_node = Node(data)
+        
+        if self.root is None:
+            self.root = new_node
+            return
+
+
+        queue = [self.root]
+        
+        while queue:
+            current = queue.pop(0)
+
+
+            if current.left is None:
+                current.left = new_node
+                return
+            else:
+                queue.append(current.left)
+
+
+            if current.right is None:
+                current.right = new_node
+                return
+            else:
+                queue.append(current.right)
+
+
+    def in_order_traversal(self, node):
+        if node is not None:
+            self.in_order_traversal(node.left)
+            print(node.data, end=" ")
+            self.in_order_traversal(node.right)
+
+
+binary_tree = BinaryTree()
+binary_tree.insert(1)
+binary_tree.insert(2)
+binary_tree.insert(3)
+binary_tree.insert(4)
+binary_tree.insert(5)
+
+print("In-order Traversal: ")
+binary_tree.in_order_traversal(binary_tree.root)
