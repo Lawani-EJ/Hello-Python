c9f3750dc1df445911a0276408f39e6a63aec159
Lawani-EJ <elyonjohn60@gmail.com>
2024-10-01 21:37:05 +0100

    Button widget implemented For this commit i implemented the button widget.

--- Tkinter/script.py
+++ Tkinter/script.py
@@ -1,5 +1,6 @@
-from tkinter import *
-root = Tk()
-w = Label(root, text="Hello-Python")
-w.pack()
-root.mainloop()
+import tkinter as tk
+r = tk.Tk()
+r.title ("Whatever you do ... Please!! Don't click the button")
+button = tk.Button(r, text="Click Me", width=28, command=r.destroy)
+button.pack()
+r.mainloop()
