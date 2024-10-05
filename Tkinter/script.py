e0b23a1aa097ea24efb543f0c5c53dbad4edcd11
Lawani-EJ <elyonjohn60@gmail.com>
2024-10-05 21:02:04 +0100

    Implementing the combobox with tkinter.

--- Tkinter/script.py
+++ Tkinter/script.py
@@ -1,14 +1,20 @@
-from tkinter import *
-root = Tk()
-menu = Menu(root)
-root.config(menu=menu)
-filemenu = Menu(menu)
-menu.add_cascade(label='File', menu=filemenu)
-filemenu.add_command(label='New')
-filemenu.add_command(label='Open....')
-filemenu.add_separator()
-filemenu.add_command(label='Exit', command=root.quit)
-helpmenu = Menu(menu)
-menu.add_cascade(label='Help', menu=helpmenu)
-helpmenu.add_command(label='About')
-mainloop()
+import tkinter as tk
+from tkinter import ttk
+
+def on_select(event):
+    selected_item = combo_box.get()
+    label.config(text="Selected Item: " + selected_item)
+
+root = tk.Tk()
+root.title("Combo Box Example..")
+
+label = tk.Label(root, text="Selected Item: ")
+label.pack(pady=10)
+
+combo_box = ttk.Combobox(root, values=["Option 1", "Option2", "Option3"])
+combo_box.pack(pady=5)
+
+combo_box.set("Option1")
+combo_box.bind("<<ComboBox Selected>>", on_select)
+
+root.mainloop()
