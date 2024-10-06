dd3bed33b883298285c89d6f99c222f89e129125
Lawani-EJ <elyonjohn60@gmail.com>
2024-10-06 23:00:19 +0100

    Implemented the scale widget with tkinter A scale is a graphical slider that allows you to select any value from that scale.

--- Tkinter/script.py
+++ Tkinter/script.py
@@ -1,20 +1,8 @@
-import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 
-def on_select(event):
-    selected_item = combo_box.get()
-    label.config(text="Selected Item: " + selected_item)
-
-root = tk.Tk()
-root.title("Combo Box Example..")
-
-label = tk.Label(root, text="Selected Item: ")
-label.pack(pady=10)
-
-combo_box = ttk.Combobox(root, values=["Option 1", "Option2", "Option3"])
-combo_box.pack(pady=5)
-
-combo_box.set("Option1")
-combo_box.bind("<<ComboBox Selected>>", on_select)
-
-root.mainloop()
+master = Tk()
+w = Scale(master, from_=0, to=42)
+w.pack()
+w = Scale(master, from_=0, to=200, orient=HORIZONTAL)
+w.pack()
+mainloop()
