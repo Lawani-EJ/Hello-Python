03306160a2056586f004081c0d37e9a169d037d4
Lawani-EJ <elyonjohn60@gmail.com>
2024-10-09 22:29:29 +0100

    Implementing progression bar with Tkinter

--- Tkinter/script.py
+++ Tkinter/script.py
@@ -1,12 +1,28 @@
-from tkinter import *
-top = Tk()
-mb = Menubutton (top, text = "Go and learn python!!")
-mb.grid()
-mb.menu = Menu (mb, tearoff = 0)
-mb["menu"] = mb.menu
-cVar = IntVar()
-aVar = IntVar()
-mb.menu.add_checkbutton (label = 'Contacts', variable=cVar)
-mb.menu.add_checkbutton (label = 'About Us', variable=aVar)
-mb.pack()
-top.mainloop()
+import tkinter as tk
+from tkinter import ttk
+import time
+
+def start_progress():
+    progress.start()
+
+    # simulating a task that takes time to complete
+    for i in range(101):
+        # simulating some work 
+        time.sleep(0.05)
+        progress['value'] = i
+        # updating the UI 
+        root.update_idletasks()
+    progress.stop()
+
+root = tk.Tk()
+root.title("Progression Bar Implementation")
+
+# creating the progression bar widget 
+progress = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
+progress.pack(pady=20)
+
+# button to start progress
+start_button = tk.Button(root, text="Click to start progression", command=start_progress)
+start_button.pack(pady=10)
+
+root.mainloop()
