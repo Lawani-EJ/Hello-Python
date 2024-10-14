fd82480e9c2a26fa92fabf34a70d2d3c7a37ee99
Lawani-EJ <elyonjohn60@gmail.com>
2024-10-14 22:43:21 +0100

    Temprature convertor using tkinter This convertor application takes the user's input Converts the temprature And displays the result.

--- Tkinter/script.py
+++ Tkinter/script.py
@@ -1,12 +1,49 @@
+# Temprature converter 
+# Author: [Lawani Elyon John]
+# Date: [10/14/2024]
+
+# Three elements are needed 
+# 1. The temperature value to be converted
+# 2. The unit of the temperature value to be converted (Celsius or Fahrenheit)
+# 3. The unit to which the temperature value is to be converted (Celsius or Fahrenheit
+
 import tkinter as tk
-root = tk.Tk()
-root.title("Pack Examples")
 
-button1 = tk.Button(root, text="This is the first button.")
-button2 = tk.Button(root, text="This is the second button.")
-button3 = tk.Button(root, text="This is the third button.")
+# Function to convert Fahrenheit to Celsius
+def perform_fahrenheit_to_celsius():
+    try:
+        fahrenheit = float(ent_temperature.get())
+        celsius = (fahrenheit - 32) * 5/9
+        lbl_result.config(text=f"{celsius:.2f} °C")
+    except ValueError:
+        lbl_result.config(text="Invalid Input")
 
-button1.pack()
-button2.pack()
-button3.pack()
-root.mainloop()
+
+window = tk.Tk()
+window.title("A Temperature Converter")
+window.resizable(False, False)
+
+
+frame_entry = tk.Frame(master=window)
+ent_temperature = tk.Entry(master=frame_entry, width=10)
+lbl_temp = tk.Label(master=frame_entry, text="°F")
+
+
+ent_temperature.grid(row=0, column=0, sticky="e")
+lbl_temp.grid(row=0, column=1, sticky="w")
+frame_entry.grid(row=0, column=0, padx=10, pady=10)
+
+
+btn_convert = tk.Button(
+    master=window,
+    text="→",
+    command=perform_fahrenheit_to_celsius
+)
+btn_convert.grid(row=0, column=1, padx=10, pady=10)
+
+
+lbl_result = tk.Label(master=window, text="°C")
+lbl_result.grid(row=0, column=2, padx=10, pady=10)
+
+
+window.mainloop()
