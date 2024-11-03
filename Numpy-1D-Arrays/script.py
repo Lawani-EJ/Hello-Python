23903492b6885d30f375cdce470860fabe32bad9
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-03 23:18:35 +0100

    Numpy 1D arrays In this commit i imolemented an element-wise and dot product calculation with numpy arrays

---
+++ Numpy-1D-Arrays/script.py
@@ -0,0 +1,7 @@
+# Class Question What is the result of the following operations
+# Provide Answers 
+import numpy as np
+result1 = np.array([1,-1]) * np.array([1,1])
+print(result1)
+result2 = np.dot(np.array([1,-1]),np.array([1,1]))
+print(result2)
