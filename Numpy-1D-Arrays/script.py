3a04110264ecf9193a591ccb9862064118140953
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-04 10:43:39 +0100

    Numpy 1D arrays performance Printing the typr of the numpy array finding the shape of the array finding the mean of the array and the type of data in the array

--- Numpy-1D-Arrays/script.py
+++ Numpy-1D-Arrays/script.py
@@ -5,3 +5,15 @@ result1 = np.array([1,-1]) * np.array([1,1])
 print(result1)
 result2 = np.dot(np.array([1,-1]),np.array([1,1]))
 print(result2)
+
+a = [1,2,3,4,5]
+# cast the following list into a numpy array 
+x = np.array(a)
+# find the type of x using the function type() 
+print(type(x))
+# find the shape of the array 
+print(x.shape)
+# find the mean of the array 
+print(x.mean)
+# find the type of data in the array 
+print(x.dtype)
