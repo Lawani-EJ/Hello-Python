3a6709b9316aa6cc5d295bf1100faf7fb5162fab
Lawani-EJ <elyonjohn60@gmail.com>
2025-01-27 15:33:14 +0100

    Making POST abd GET requests with the python requests library This commits add functionality to make POST and GET requests using the requests library Makes a POST request to a specified URL with JSON data in the request body. Makes a GET request to a URL with queries using string params .

--- HTTPSandRequests/script.py
+++ HTTPSandRequests/script.py
@@ -18,3 +18,20 @@ print(r.headers['date'])
 print(r.headers['Content-Type'])
 print(r.encoding)
 print(r.text[0:100])
+
+# making a POST request
+url2 = 'https://www.ibm.com'
+r2 = requests.post(url2, json = {'content': 'Hello, World!'})
+print(r2.status_code)
+
+
+# making a GET request with URL paremeters (params)
+url_get = 'http://httpbin.org/get'
+# add a query string
+# ? -> parameter name -> = -> value -> & -> parameter name -> = -> value
+payload = {"name": "Joseph", "ID": "123"}
+r3 = requests.get(url_get,params=payload)
+print(r3.url)
+print("request body: ", r3.request.body)
+print(r3.status_code)
+print(r3.text)
