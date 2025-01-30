0a34ab1f61ade873f2bcf7971787c6a1f82878e5
Lawani-EJ <elyonjohn60@gmail.com>
2025-01-30 09:37:11 +0100

    Refactor: Improving user display and data retrival

--- APIUsage/script.py
+++ APIUsage/script.py
@@ -4,3 +4,10 @@ import pandas as pd
 r = RandomUser()
 some_list = r.generate_users(10)
 print(some_list)
+
+name = r.get_full_name()
+print(name)
+
+# 10 users with their full name and email adress
+for user in some_list:
+    print(user.get_full_name(), user.get_email())
